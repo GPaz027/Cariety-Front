4a4983e7ae33eb7f40fa248a2a4023e2c9549493
NicoCousino <nicocorpaorations@gmail.com>
2023-08-12 17:38:53 -0300

    feat: Connection complete

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { useEffect, useState } from "react";
 
-
-
-
 export default function Main() {
 
     const URL = "ws://localhost:3100/ws"
 
-    const URL_PREDICCION = "http://127.0.0.1:300/image/"
+    const URL_PREDICCION = "http://127.0.0.1:3000/predict"
 
     const webSocket = new WebSocket(URL);
 
@@ -16,21 +13,31 @@ export default function Main() {
     const [image, setImage] = useState("https://es.vitejs.dev/logo.svg")
     const [n, setN] = useState(0) 
 
-    webSocket.onopen = (event) =>{console.log("Websocket iniciado")}
+    useEffect(()=>{
+        webSocket.onopen = (event) => { console.log("Websocket iniciado") }
 
+        webSocket.onmessage = (event) => {
+            console.log(event.data)
+            setText(event.data)
+            if (event.data === "Imagen Disponible"){
+                getResult()
+            }
+        }
+
+    }, [])
+
+    //webSocket.onopen = (event) =>{console.log("Websocket iniciado")}
+
+    /*
     webSocket.onmessage = (event) => {
         console.log(event.data)
         setText(event.data)
     }
+    */
 
-    const click = ()=>{
-        setText("El valor es " + n)
-        setN(n+1)
-        getResult()
-    }
+    const click = ()=>{ setText("El valor es " + n); setN(n+1); getResult() }
 
     const getResult = () => {
-        //const data = { "user_input": "./Imagenes/" } 
         console.log("buscando")
         fetch(URL_PREDICCION, {
             method: 'GET', 
@@ -38,7 +45,10 @@ export default function Main() {
               'Content-Type': 'application/json'
             },
             //body: JSON.stringify(data) 
-          }).then(r => r.json().then((r) => {setN(100)}))//setImage(r.image_base64[0]); console.log(r)}))
+          }).then(response => response.json().then((r) => {
+            setText(r.image)
+            console.log("Exito!")
+          }))
     }
 
 
--- src/components/express.js
+++ src/components/express.js
@@ -1,14 +1,19 @@
 import express from "express";
-import server from "http"
-import WebSocket, { WebSocketServer } from "ws";
+import { WebSocketServer } from "ws";
+import cors from "cors";
 
+
+/* ------------------------------- DEFINICION DE VARIABLES -------------------------------*/
 const app = express()
-const port = 3000
-const wsPort = 3100
+const expressPort = 3000
 
+const wsPort = 3100
 const wss = new WebSocketServer({port: wsPort})
 
+let image = "Soy la Imagen"
 
+
+/* ------------------------------- METODOS DEL WEBSOCKET -------------------------------*/
 wss.on('connection', function connection(ws) {
     console.log("Websocket del microbackend encendido")
 
@@ -17,31 +22,27 @@ wss.on('connection', function connection(ws) {
     ws.on('message', function message(data) {
         console.log('Me active');
         ws.send("Imagen Lista")
-        
-
     });
-  });
+});
 
+/* ------------------------------- ENDPOINTS DE EXPRESS -------------------------------*/
 
+app.use(cors({ origin: 'http://localhost:5173' }));
 
-/*
-wss.addListener('imageReady', function connection(ws) {
-    ws.send("La imagen esta Lista")
-})
-*/
-
-
-
-
-app.listen(port, ()=>{console.log("Port is working")})
+app.listen(expressPort, ()=>{console.log("Port is working")})
 
 
 app.get('/image', (req, res) => {
-        //wss.emit('connection')
-        wss.clients.forEach((client)=>{ client.send("Imagen Disponible")})
-        res.send('Active la Disponibilidad de la Imagen')
-    })
+    wss.clients.forEach((client)=>{ client.send("Imagen Disponible")})
+    image = image + " I "
+    res.send('Active la Disponibilidad de la Imagen')
+})
 
-    app.get('/', (req, res) => {
-        res.send('Imagen a Procesar')
-    })
+app.get('/', (req, res) => {
+    res.send('Imagen a Procesar')
+})
+
+app.get("/predict", (req, res)=>{
+    res.send({"image": image})
+
+})
